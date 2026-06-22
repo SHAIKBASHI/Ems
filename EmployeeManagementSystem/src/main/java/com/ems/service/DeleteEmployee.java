df98bc7ecddcafc74c68ba03d6d102d1d30c8881
Bashi <bashiscrap@gmail.com>
2026-06-22 16:32:45 +0530

    first version

---
+++ EmployeeManagementSystem/src/main/java/com/ems/service/DeleteEmployee.java
@@ -0,0 +1,42 @@
+package com.ems.service;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ems.dao.EmployeeDAO;
+import com.ems.model.Employee;
+
+@WebServlet("/delete")
+public class DeleteEmployee extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		int id=(Integer.parseInt(request.getParameter("id")));
+		
+
+		EmployeeDAO dao = new EmployeeDAO();
+
+		boolean status = dao.deleteEmployee(id);
+
+		RequestDispatcher rd = request.getRequestDispatcher("findAll");
+		if (status) {
+			dao.commit();
+			request.setAttribute("status", "deleted succesfully");
+			rd.forward(request, response);
+		} else {
+			dao.rollback();
+			
+			request.setAttribute("status", "Error in deleteing  employee");
+			rd.forward(request, response);
+		}
+
+	}
+
+}
